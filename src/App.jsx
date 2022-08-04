53c8fd705dd4868d1febf15e489ce93512a6ed79
LEONIDAS <jesus.leon1@unmsm.edu.pe>
2022-08-04 13:42:57 -0500

    feat: add inputForm component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import './App.css';
+import { useState } from 'react';
+import './app.css';
+import FormInput from './components/formInput/FormInput';
 
 const App = () => {
+  const [username, setUsername] = useState("")
+  
+  console.log(username)
+
   return(
-    <div>
-      React Form
+    <div className="app">
+      <form>
+        <FormInput placeholder="Username" setUsername={setUsername}/>
+        <FormInput placeholder="Email" setUsername={setUsername}/>
+        <FormInput placeholder="Full name" setUsername={setUsername}/>
+        <FormInput placeholder="Sth else" setUsername={setUsername}/>
+      </form>
     </div>
   )
 }
